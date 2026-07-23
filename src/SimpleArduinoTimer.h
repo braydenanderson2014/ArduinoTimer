941058138727ef3334aa5a2babb06524e4a6bafa
Brayden Anderson <braydenanderson2014@gmail.com>
2026-07-22 22:04:55 -0600

    update to workflows

---
+++ src/SimpleArduinoTimer.h
@@ -0,0 +1,53 @@
+#ifndef SIMPLEARDUINOTIMER_h
+#define SIMPLEARDUINOTIMER_h
+
+#include <Arduino.h>
+#include <RTClib.h>
+
+class Timer {
+private:
+    unsigned long startTime;
+    unsigned long elapsedTime;
+    unsigned long pauseTime;
+    unsigned long targetDuration;
+    bool isRunning;
+    bool isPaused;
+    bool useRTC;
+    mutable RTC_DS3231 rtc;
+    bool debug;
+    unsigned long remainingTimeOnTimer;
+    enum mode {Seconds, Minutes, Hours};
+    mode timerMode = Minutes;
+    String TimerName;
+public:
+    Timer(bool debug);
+    void setTimerName(String timerName);
+    String getTimerName() const;
+    void updateRemainingTime();
+    void start();
+    void stop();
+    void reset();
+    void clear();
+    void pause();
+    void resume();
+    void setUseRTC(bool useRTC);
+    void begin();
+    void printTimeRemaining();
+    unsigned long elapsed();
+    bool isTimerRunning() const;
+    bool hasReachedTarget();
+    bool getUseRTC() const;
+    unsigned long remainingTime();
+
+    void syncWithRTC();
+    DateTime getRTCTime() const;
+    bool isTimerPaused() const;
+    void setRTCTime(int year, int month, int day, int hour, int minute, int second);
+    void setTargetDuration(unsigned long duration);
+    void setTargetSeconds(unsigned long seconds);
+    void setTargetMinutes(unsigned long minutes);
+    void setTargetHours(unsigned long hours);
+    bool checkTimer(unsigned long duration);
+};
+
+#endif // SIMPLEARDUINOTIMER_h
